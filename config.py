4d8204969ff86815fa50ba25c776f7b727b1afeb
Gaikwad Kunal <118621248+dor3Monbotz@users.noreply.github.com>
2023-05-07 11:22:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import environ
+
+
+class Config(object):
+    API_ID = int(environ.get("API_ID", 0))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    
